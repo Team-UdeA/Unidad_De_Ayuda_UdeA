cda77a897f313a9e6d43d5dc5dbcd261d415d289
Jonatan_UdeA <jonatan.romeroa@udea.edu.co>
2023-10-21 16:09:53 -0500

    Proyecto 0/100

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,8 +51,8 @@ export default function RootLayout({
 								href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
 								title="nextui.org homepage"
 							>
-								<span className="text-default-600">Powered by</span>
-								<p className="text-primary">NextUI</p>
+								<span className="text-default-600">Powered Teams</span>
+								<p className="text-primary">UdeA</p>
 							</Link>
 						</footer>
 					</div>
