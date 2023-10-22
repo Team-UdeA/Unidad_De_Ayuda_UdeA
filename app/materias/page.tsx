eeb744dd0071f2e9b2b14bf8e40060ea056ffd59
Jonatan_UdeA <jonatan.romeroa@udea.edu.co>
2023-10-22 14:31:22 -0500

    zoom al componente de imagen

--- app/materias/page.tsx
+++ app/materias/page.tsx
@@ -1,4 +1,7 @@
 import { title } from "@/components/primitives";
+import React from "react";
+import {Image} from "@nextui-org/image";
+
 const callouts = [
 	{
 	  name: 'Calculo Diferencia',
@@ -53,12 +56,13 @@ const callouts = [
 			<div className="mt-6 space-y-12 lg:grid lg:grid-cols-5 lg:gap-x-6 lg:space-y-0">
 				{callouts.map((callout) => (
 					<div key={callout.name} className="group relative">
-					<div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-						<img
+					<div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1  sm:h-64">
+						 <Image
+						isZoomed
+						width={240}
 						src={callout.imageSrc}
 						alt={callout.imageAlt}
-						className="h-full w-full object-cover object-center"
-						/>
+    					/>
 					</div>
 					<h3 className="mt-6 text-sm text-gray-500">
 						<a href={callout.href}>
